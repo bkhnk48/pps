cabf57d7bce1a66e7357a590d5d6856f50b6adb5
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2025-05-27 02:25:20 +0700

    remove redundant method and attributes

--- controller/reading_input_processor.py
+++ controller/reading_input_processor.py
@@ -8,7 +8,7 @@ from openpyxl import load_workbook
 import math
 import numpy as np
 import pdb
-
+from controller.start_node_generator import StartNodeGenerator
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -20,8 +20,9 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     
-class ReadingInputProcessor:
+class ReadingInputProcessor(StartNodeGenerator):
     def __init__(self, _dm):
+        super().__init__()
         self.logger = Logger()
         self._print_out = True
         #self.x = {}
@@ -33,15 +34,8 @@ class ReadingInputProcessor:
         self.processed_numbers = []
         self._space_edges = []
         self._draw = 0
-        self._H = 0
         self._d = 0
-        self._M = 0
         self._num_max_agvs = 0
-        self._ID = []
-        self._earliness = 0
-        self._tardiness = 0
-        self._started_nodes = []
-        self._seed = 0
 
     # Getter và Setter cho print_out
     @property
@@ -53,40 +47,6 @@ class ReadingInputProcessor:
         if not isinstance(value, bool):
             raise ValueError("print_out must be a boolean")
         self._print_out = value
-            
-    # Getter and Setter for ID
-    @property
-    def ID(self):
-        return self._ID
-    
-    @ID.setter
-    def ID(self, value):
-        self._ID = value
-    
-    # Getter and Setter for earliness
-    @property
-    def earliness(self):
-        return self._earliness
-    
-    @earliness.setter
-    def earliness(self, value):
-        self._earliness = value
-    
-    # Getter and Setter for tardiness
-    @property
-    def tardiness(self):
-        return self._tardiness
-
-    @tardiness.setter
-    def tardiness(self, value):
-        self._tardiness = value    
-    
-    @property
-    def M(self):
-        return self._M
-    @M.setter
-    def M(self, value):
-        self._M = value
     
     # Getter and Setter for d
     @property
@@ -96,15 +56,6 @@ class ReadingInputProcessor:
     @d.setter
     def d(self, value):
         self._d = value
-
-    # Getter and Setter for H
-    @property
-    def H(self):
-        return self._H
-
-    @H.setter
-    def H(self, value):
-        self._H = value
         
     @property
     def draw(self):
@@ -123,28 +74,6 @@ class ReadingInputProcessor:
         if not isinstance(value, list):
             raise ValueError("space_edges must be a list")
         self._space_edges = value
-        
-    # Getter và Setter cho started_nodes
-    @property
-    def started_nodes(self):
-        return self._started_nodes
-
-    @started_nodes.setter
-    def started_nodes(self, value):
-        if not isinstance(value, list):
-            raise ValueError("started_nodes must be a list")
-        self._started_nodes = value
-        
-    # Getter và Setter cho seed
-    @property
-    def seed(self):
-        return self._seed
-
-    @seed.setter
-    def seed(self, value):
-        if not isinstance(value, int):
-            raise ValueError("seed must be an integer")
-        self._seed = value
         
     # Getter và Setter cho num_max_agvs
     @property
@@ -386,26 +315,7 @@ class ReadingInputProcessor:
         num_of_agvs = self.num_max_agvs
         config.num_max_agvs = self.num_max_agvs
         config.numOfAGVs = num_of_agvs
-        if len(self.started_nodes) == 0:
-            self.ID = []
-            self.earliness = []
-            self.tardiness = []
-            #pdb.set_trace()
-            for _ in range(num_of_agvs):
-                [s, d, e, t] = self.generate_numbers_student(self.M, self.H, int(0.2*self.M))#, 100 if self.H > 100 else self.H//3)
-                while s in self.started_nodes:
-                    s += self.M
-                    if s >= self.H * self.M:
-                        break
-                self.started_nodes.append(s)
-                self.ID.append(d)
-                self.earliness.append(e)
-                self.tardiness.append(t)
-            print(f'Start: {self.started_nodes} \n End: {self.ID} \n Earliness: {self.earliness} \n Tardiness: {self.tardiness}')
-            config.started_nodes = self.started_nodes.copy()
-            config.ID = self.ID.copy()
-            config.earliness = self.earliness.copy()
-            config.tardiness = self.tardiness.copy()
+        self.generate_start_nodes(num_of_agvs)
         return num_of_agvs
     
     def generate_new_agvs(self):
@@ -444,21 +354,5 @@ class ReadingInputProcessor:
         else:
             num_of_agvs = self.generate_time_windows()
         return num_of_agvs
-            
-    def generate_numbers_student(self, G, H, M, N = 0, df=10):
-        while True:
-            self._seed = self._seed + 1
-            self._seed = self._seed % G
-            np.random.seed(self._seed)
-            # Sinh 4 số ngẫu nhiên theo phân phối Student
-            first_two = np.random.standard_t(df, size=2)
-            numbers = np.random.standard_t(df, size=2)
-            # Chuyển đổi các số này thành số nguyên trong khoảng từ 1 đến 100
-            first_two = np.round((first_two - np.min(first_two)) / (np.max(first_two) - np.min(first_two)) * (G//3) + self._seed).astype(int)
-            numbers = np.round((numbers - np.min(numbers)) / (np.max(numbers) - np.min(numbers)) * (H//3) + self._seed).astype(int)
-            if first_two[0] < G and first_two[1] < G and numbers[0] <= numbers[1] and numbers[1] < H:
-                # Kiểm tra điều kiện khoảng cách tối thiểu
-                if (abs(first_two[0] - first_two[1]) >= M and abs(numbers[0] - numbers[1]) >= N):
-                    return np.concatenate((first_two, numbers))
                 
     
