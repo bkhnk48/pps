99a91f5d97b0e9a7e08cce5b79abbf29a5b2ccbf
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2025-05-27 19:27:07 +0700

    declare a class

--- config.py
+++ config.py
@@ -21,3 +21,14 @@ test_automation = 0
 draw = 0
 M = 0
 print_output = False
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
