2310b8e8d8c4dd97c8d8d9fdefc8f5393cac0464
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2025-07-22 11:35:39 +0700

    two new functions

--- controller/restriction_integrator_tqm.py
+++ controller/restriction_integrator_tqm.py
@@ -1,6 +1,6 @@
 from controller.NodeGenerator import ArtificialNode
 from controller.RestrictionController import RestrictionController
-from controller.MaxFlowPipeline import MaxFlowPipeline
+from controller.max_flow_pipeline import MaxFlowPipeline
 from collections import defaultdict
 from model.Graph import Graph
 from typing import List, Tuple, Set, Optional, Dict
@@ -17,7 +17,7 @@ class RestrictionIntegratorTQM(RestrictionController):
             return config.max_flow_conditions or [(1, 2, 3, 4)]
         conditions = []
         while True:
-            line = input("Nhập các bộ điều kiện x y a b (default: '1 2 3 4'): ").strip()
+            line = input("Nhập các bộ điều kiện x y a b (default: '1 2 3 4') - Enter để dừng nhập: ").strip()
             if not line:
                 break
             try:
@@ -40,12 +40,28 @@ class RestrictionIntegratorTQM(RestrictionController):
     def insert_artificial_objects(self, F, U=None, gamma=None, use_config_data=False):
         """Chèn node/cung ảo nếu cần thiết."""
         if U is None:
-            U = get_artificial_upper_bound(use_config_data)
+            U = self.get_artificial_upper_bound(use_config_data)
         if gamma is None:
-            gamma = get_artificial_gamma(use_config_data)
+            gamma = self.get_artificial_gamma(use_config_data)
         #print(f"✅ Max Flow F = {F}, U = {U}")
         import controller.ArtificialNodeInserter
         if F > U and F != 0:
             if self.graph_processor.graph is None:
                 self.graph_processor.graph = Graph(self.graph_processor)
             ArtificialNodeInserter(self.graph_processor).run(U, gamma)
+            
+    def get_artificial_upper_bound(self, use_config_data=False):
+        if config.artificial_upper_bound is not None or use_config_data:
+            return config.artificial_upper_bound or 1
+        U_input = input("Nhập U (default: 1): ")
+        U = int(U_input) if U_input.strip() else 1
+        config.artificial_upper_bound = U
+        return U
+
+    def get_artificial_gamma(self, use_config_data=False):
+        if config.artificial_gamma is not None or use_config_data:
+            return config.artificial_gamma or 1230919231
+        gamma_input = input("Nhập gamma (default: 1230919231): ")
+        gamma = int(gamma_input) if gamma_input.strip() else 1230919231
+        config.artificial_gamma = gamma
+        return gamma
