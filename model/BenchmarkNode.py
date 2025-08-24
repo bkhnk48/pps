c65e1255a3df989228dbd02e2956a677ca3440b3
MKK2K4 <63004324+thieuminh@users.noreply.github.com>
2025-08-25 02:22:22 +0700

    Update model\BenchmarkNode.py

--- model/BenchmarkNode.py
+++ model/BenchmarkNode.py
@@ -4,6 +4,12 @@ class BenchmarkNode(Node):
     def __init__(self, id, label=None):
         super().__init__(id, label)
         
+    def __repr__(self):
+        cls = self.__class__.__name__
+        lbl = getattr(self, "label", cls)
+        tmp = getattr(self, "temporary", False)
+        return f"{cls}(id={self.id}, label='{lbl}', temporary={tmp})"
+
 class TopBulbNode(BenchmarkNode):
     def __init__(self, id):
         super().__init__(id, "TopBulb")
