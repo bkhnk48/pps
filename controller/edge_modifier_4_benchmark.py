5e4d9b8fbe2226932b50ebd9807480eed4382b68
MKK2K4 <63004324+thieuminh@users.noreply.github.com>
2025-09-03 20:01:30 +0700

    Update controller\edge_modifier_4_benchmark.py

--- controller/edge_modifier_4_benchmark.py
+++ controller/edge_modifier_4_benchmark.py
@@ -3,28 +3,35 @@ from model.Node import Node
 
 
 class EdgeModifier4Benchmark(EdgeModifier):
+	# -------- Overrides --------
 	def insertEdgesAndNodes(self, start, end, edge):
 		fmt = getattr(self, "_input_format", None)
-
-		if fmt == 'benchmark':
-			start_id = start if isinstance(start, int) else start.id
-			end_id = end if isinstance(end, int) else end.id
-
-			if start_id not in self.graph.adjacency_list:
-				self.graph.adjacency_list[start_id] = []
-			if end_id not in self.graph.adjacency_list:
-				self.graph.adjacency_list[end_id] = []
-
-			# Append the directed edge
-			self.graph.adjacency_list[start_id].append((end_id, edge))
-
-			# Resolve Node objects and ensure node dict entries exist
-			start_node = start if isinstance(start, Node) else self.find_node(start)
-			end_node = end if isinstance(end, Node) else self.find_node(end)
-
-			if start_id not in self.graph.nodes or self.graph.nodes[start_id] is None:
-				self.graph.nodes[start_id] = start_node
-			if end_id not in self.graph.nodes or self.graph.nodes[end_id] is None:
-				self.graph.nodes[end_id] = end_node
-		else:
+		if fmt != 'benchmark':
 			return super().insertEdgesAndNodes(start, end, edge)
+
+		start_id, end_id = self._get_ids(start, end)
+		self._ensure_buckets(start_id, end_id)
+		self.graph.adjacency_list[start_id].append((end_id, edge))
+		self._ensure_nodes(start_id, end_id, start, end)
+
+	# -------- Helpers --------
+	def _get_ids(self, start, end):
+		start_id = start if isinstance(start, int) else start.id
+		end_id = end if isinstance(end, int) else end.id
+		return start_id, end_id
+
+	def _ensure_buckets(self, start_id, end_id):
+		adj = self.graph.adjacency_list
+		if start_id not in adj:
+			adj[start_id] = []
+		if end_id not in adj:
+			adj[end_id] = []
+
+	def _ensure_nodes(self, start_id, end_id, start, end):
+		nodes = self.graph.nodes
+		if start_id not in nodes or nodes[start_id] is None:
+			start_node = start if isinstance(start, Node) else self.find_node(start)
+			nodes[start_id] = start_node
+		if end_id not in nodes or nodes[end_id] is None:
+			end_node = end if isinstance(end, Node) else self.find_node(end)
+			nodes[end_id] = end_node
