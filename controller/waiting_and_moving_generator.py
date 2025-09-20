cf717837f575284131a048d6548a5e4053d92bf5
MKK2K4 <63004324+thieuminh@users.noreply.github.com>
2025-09-20 16:43:44 +0700

    Update waiting_and_moving_generator.py

--- controller/waiting_and_moving_generator.py
+++ controller/waiting_and_moving_generator.py
@@ -1,10 +1,11 @@
 from controller.NodeGenerator import TimeWindowNode
 from controller.time_window_generator import TimeWindowGenerator
+from controller.time_window_generator_4_benchmark import TimeWindowGenerator4Benchmark
 from controller.rerouting_controller import ReroutingController
 import pdb
 
 #Sẽ được lớp EdgeModifier kế thừa
-class WaitingAndMovingEdgesGenerator (TimeWindowGenerator):
+class WaitingAndMovingEdgesGenerator (TimeWindowGenerator4Benchmark):
     def __init__(self, dm):
         super().__init__(dm) 
         self._graph = None
