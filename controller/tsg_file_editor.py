7a5fb2455a222d27aa73e844a1d9b8dc7657f5e1
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2025-09-20 15:27:02 +0700

    Update tsg_file_editor.py

--- controller/tsg_file_editor.py
+++ controller/tsg_file_editor.py
@@ -1,10 +1,9 @@
 from controller.reading_input_processor import ReadingInputProcessor
-from controller.reading_benchmark_map_processor import ReadingBenchmarkMapProcessor
 from model.Node import Node
 from controller.NodeGenerator import TimeoutNode
 
 #Sẽ được lớp TimeWindowGenerator kế thừa
-class TsgFileEditor(ReadingBenchmarkMapProcessor):
+class TsgFileEditor(ReadingInputProcessor):
     def __init__(self, dm):
         super().__init__(dm) 
         self._target_nodes = []
