a14949a20c69398e868f0f2f15c642b57853db4a
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2025-09-20 15:30:13 +0700

    use rerouting_controller

--- controller/waiting_and_moving_generator.py
+++ controller/waiting_and_moving_generator.py
@@ -1,13 +1,14 @@
 from controller.NodeGenerator import TimeWindowNode
 from controller.time_window_generator import TimeWindowGenerator
-from controller.time_window_generator_4_benchmark import TimeWindowGenerator4Benchmark
+from controller.rerouting_controller import ReroutingController
 import pdb
 
 #Sẽ được lớp EdgeModifier kế thừa
-class WaitingAndMovingEdgesGenerator (TimeWindowGenerator4Benchmark):
+class WaitingAndMovingEdgesGenerator (TimeWindowGenerator):
     def __init__(self, dm):
         super().__init__(dm) 
         self._graph = None
+        self.rerouting_controller = ReroutingController(self)
 
     # Getter và Setter cho graph
     @property
@@ -126,46 +127,11 @@ class WaitingAndMovingEdgesGenerator (TimeWindowGenerator4Benchmark):
         self.create_set_of_edges(new_a)
     def write_to_file(self, agv_id_and_new_start=None, new_halting_edges=None,
                   supply=None, vs_id=None, vt_id=None, filename="TSG.txt"):
-        get_targets = self.get_targets \
-            if hasattr(self, 'get_targets') \
-                else self.graph_processor.get_targets
-        targets = get_targets()
-        M = max(target.id for target in targets)
-        if new_halting_edges: M = max(M, max(e[1] for e in new_halting_edges))
-        num_edges = (self.count_edges() \
-            if hasattr(self, 'count_edges') \
-                else len(self.ts_edges)) + (len(new_halting_edges) if new_halting_edges else 0)
-
-        with open(filename, 'w') as f:
-            f.write(f"p min {M} {num_edges}\n")
-            starts = self.started_nodes#getattr(self, 'started_nodes', self.graph.getAllNewStartedNodes())
-            self._write_node_lines(f, starts, targets, supply, vs_id, vt_id)
-
-            if hasattr(self, 'ts_edges'):
-                for e in self.ts_edges: self._write_edge_lines(f, e)
-            elif hasattr(self, 'adjacency_list'):
-                for sid, edges in sorted(self.adjacency_list.items()):
-                    for eid, data in edges:
-                        f.write(f"a {sid} {eid} {data.lower} {data.upper} {data.weight}\n")
-
-            if new_halting_edges:
-                for e in new_halting_edges:
-                    f.write(f"a {e[0]} {e[1]} {e[2]} {e[3]} {e[4]}\n")
-
-        if getattr(self, "print_out", False):
-            print("Đã cập nhật các cung mới vào file TSG.txt.")
-
-    def _write_edge_lines(self, f, edge):
-        if edge is None: return
-        if edge.weight == self.H * self.H \
-            and (edge.start_node.id // self.M - (edge.start_node.id % self.M == 0)) >= self.H:
-            f.write(f"c Exceed {edge.weight} {edge.weight // self.M}\n")
-        f.write(f"a {edge.start_node.id} {edge.end_node.id} "
-            f"{edge.lower} {edge.upper} {edge.weight}\n")
-        
-    def _write_node_lines(self, f, starts, targets, supply, vs_id, vt_id):
-        for s in starts:
-            f.write(f"n {s} {supply if supply and vs_id == s else 1}\n")
-        for t in targets:
-            f.write(f"n {t.id} {-supply if supply and vt_id == t.id else -1}\n")
+        if agv_id_and_new_start is not None:
+            if agv_id_and_new_start[1] == 855:
+                pdb.set_trace()
+        self.rerouting_controller.write_to_file(
+            agv_id_and_new_start, new_halting_edges,
+            supply, vs_id, vt_id, filename
+        )
 
