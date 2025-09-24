8d758538cfd586f021b59a8393f9bc7c4ef77a24
MKK2K4 <63004324+thieuminh@users.noreply.github.com>
2025-09-25 00:26:09 +0700

    Update TimeSpaceGraph4Benchmark.py

--- controller/TimeSpaceGraph4Benchmark.py
+++ controller/TimeSpaceGraph4Benchmark.py
@@ -1,4 +1,4 @@
-from model.BenchmarkNode import BenchmarkNode, TopBulbNode, BottomBulbNode, BottleneckNode
+from model.BenchmarkNode import BenchmarkNode, BottleneckNode, TopOrBottomBulbNode
 from model.BenchmarkEdge import BenchmarkEdge, InflowEdge, NeckEdge, OutflowEdge, WaitingEdge
 import os
 
@@ -50,8 +50,8 @@ class TimeSpaceGraph4Benchmark:
 
     def create_nodes(self, a1, a2, a3, a4, v1, v2):
         nodes = [
-            TopBulbNode(a1), TopBulbNode(a3),
-            BottomBulbNode(a2), BottomBulbNode(a4),
+            TopOrBottomBulbNode(a1), TopOrBottomBulbNode(a3),
+            TopOrBottomBulbNode(a2), TopOrBottomBulbNode(a4),
             BottleneckNode(v1), BottleneckNode(v2)
         ]
         for node in nodes:
