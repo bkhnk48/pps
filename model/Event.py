6d77d618019dd989a98963e646a3b01d9f0e400f
MKK2K4 <63004324+thieuminh@users.noreply.github.com>
2025-10-25 03:45:42 +0700

    Update model\Event.py
    Call write_trace from EdgeModifier4Benchmark class

--- model/Event.py
+++ model/Event.py
@@ -175,9 +175,11 @@ class Event:
             if edge[3] == '0' and int(edge[4]) >= 1
         }
         nx.M = self.graph.graph_processor.M
-        #Di chuyển phương thức write_trace sang lớp Graph_Processor 
-        #write_trace sẽ nhận tham số là nx.flowDict để ghi ra file traces.txt
         nx.write_trace()
+        #Di chuyển phương thức write_trace sang lớp EdgeModifier4Benchmark của file edge_modifier_4_benchmark.py
+        #write_trace sẽ nhận tham số là nx.flowDict để ghi ra file traces.txt
+        # Gọi write_trace từ graph_processor như ghi chú mô tả, truyền vào nx.flowDict để ghi ra traces.txt
+        self.graph_processor.write_trace(nx.flowDict)
 
     def finalize_solution(self):
         """ Finalize solution by updating graph version. """
